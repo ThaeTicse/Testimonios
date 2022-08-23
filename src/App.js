1655be64c3bd3c429d5d599fddedc5a51e2e6520
unknown <thaedeveloper@gmail.com>
2022-08-23 00:36:36 -0500

    realizando algunos retoques en la componente y styles

--- src/App.js
+++ src/App.js
@@ -7,7 +7,30 @@ function App() {
       
         <div className='contenedor-principal'>
           <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-          <Testimonio/>
+          <Testimonio
+            nombre='Shawn Wang'
+            pais='Singapur'
+            imagen='shawn-wang'
+            cargo='Ingeniero de software'
+            empresa='Amazon'
+            testimonio={['Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando en los cientos de horas de lecciones gratuitas en freeCodeCamp. En un año tenía un trabajo de seis cifras como ingeniero de software. ', <strong>freeCodeCamp cambió mi vida.</strong>,]}/>
+          
+          <Testimonio
+            nombre='Sara Chima'
+            pais='Nigeria'
+            imagen='Sarah-Chima'
+            cargo='Ingeniero de software'
+            empresa='ChatDesk'
+            testimonio={[<strong>freeCodeCamp fue la puerta de entrada a mi carrera</strong> ,' como desarrollador de software. El plan de estudios bien estructurado llevó mi conocimiento de codificación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.']}/>  
+          
+          <Testimonio
+            nombre='Emma Bostian'
+            pais='Suecia'
+            imagen='Emma-Bostian'
+            cargo='Ingeniero de software'
+            empresa='Spotify'
+            testimonio={['Siempre me ha costado aprender JavaScript. Tomé muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en ', <strong>freeCodeCamp me dio las habilidades</strong> ,' y la confianza que necesitaba para conseguir el trabajo de mis sueños como un ingeniero de software en Spotify.']}/>
+          
         </div>
 
     </div>
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,17 +1,23 @@
 import React from "react";
 import '../styles/Testimonio.css'
 
-function Testimonio(){
+function Testimonio(props){
 
   return(
     <div className='contenedor-testimonio'>
       <img  className='imagen-testimonio' 
-            src={require('../imagenes/Emma-Bostian.png')} 
+            src={require(`../imagenes/${props.imagen}.png`)} 
             alt ='Foto de Emma'/>
-      <div className='contenedor-text'>
-        <p className='nombre'>Emma Bostian en Suecia</p>
-        <p className='cargo'>Ingeniera de Software en Spotify</p>
-        <p className='texto'>"Siempre me ha costado aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."</p>
+      <div className='contenedor-texto'>
+        <p className='nombre'> 
+          <strong>{props.nombre}</strong> en {props.pais}
+        </p>
+        <p className='cargo'>
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className='texto'>
+          "{props.testimonio}"
+        </p>
       </div>
     </div>  
     )
